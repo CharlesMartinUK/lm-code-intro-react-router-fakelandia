86a844a227e4d20732ea2e0ea53ca6ff280f5376
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-18 20:27:38 +0100

    Added misdemeanour display, very messy but works

--- client/src/components/misdemeanour/misdemeanour.tsx
+++ client/src/components/misdemeanour/misdemeanour.tsx
@@ -1,3 +1,96 @@
-const Misdemeanour : React.FC = () => <>Misdemeanour!</>
+
+import {useState, createContext, useContext  } from 'react'
+
+import {useFetchData} from '../../hooks/use_fetch_data'
+
+export type MisdemeanourDetail = { citizenId: string, misdemeanour:string, date:string  };
+
+/*
+console.log(data)
+	return (<>Misdemeanour!
+		 <UserContext.Provider value={mis}>
+			 {
+				 
+			 }
+				
+		</UserContext.Provider>
+
+
+*/
+
+interface PassedMis  {'misdemeanours':MisdemeanourDetail[] }
+
+
+function getRandomInt(max:number) {
+  return Math.floor(Math.random() * max);
+}
+
+
+const Misdemeanour : React.FC = () => {
+	
+	
+	//console.log("Headach")
+	
+	const { data, error, isFetching, status } = useFetchData<PassedMis>(`http://localhost:8080/api/misdemeanours/10`);
+	
+	//const mis = useState(data?.misdemeanours)
+	
+	//const UserContext = createContext(mis);
+	
+	//console.log(mis)
+	var a = 0
+	
+	let mds:MisdemeanourDetail[] = []
+	// STUPID CODE TO GET RID OF MISDEMANOUR KEY SO JUST HAVE ARRAY, MUST BE BETTER WAY
+	if(data) {
+		if(data.hasOwnProperty('misdemeanours') ) {
+			mds = data.misdemeanours
+			//console.log(mds)
+		}
+	}
+	
+	
+	let [filterOptions, setFilterOptions] = useState("all") 
+	
+	function change(event: React.FormEvent) {
+		// Use cast to any works but is not type safe
+        var unsafeSearchTypeValue = ((event.target) as any).value;
+		//console.log(unsafeSearchTypeValue)
+		setFilterOptions(unsafeSearchTypeValue) // note this will cause a page reload apart from state
+	}
+	
+	
+	const mis = useState(mds)
+	
+	
+	//console.log(filterOptions)
+	
+	return (<>Misdemeanour!
+			<table className='misTable'><thead><tr><th>Citizen Id</th><th>Misdemeanour</th><th>Date</th><th>Punishment</th></tr></thead>
+			<tbody>
+			 { mds.map ((m,i) => {
+					const w = getRandomInt(50)+50
+					const h = getRandomInt(50)+50
+					//console.log(w)
+					const path = "https://picsum.photos/"+w+"/"+h
+					//console.log(path)
+					if((filterOptions == 'all') || (filterOptions === m.misdemeanour))
+						return <tr key={i}><td>{m.citizenId}</td><td>{m.misdemeanour}</td><td>{ m.date }</td><td><img src={path} />;</td></tr>
+				} )
+				 
+			 }
+			 </tbody>
+			</table>	
+			
+			<select onChange={change}>
+				<option value="all" defaultValue="all" >Show all</option>
+				<option value="lift">lift</option>
+				<option value="united">United</option>
+				<option value="vegetables">Vegetables</option>
+				<option value="rudeness">Rudeness</option>
+			</select>
+			
+	</>)
+}
 
 export default Misdemeanour
