a75d20377be1626ac46f12dd5091b5bf43b29f80
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-18 15:58:37 +0100

    Menu now horizontal instead of vertical

--- client/src/components/header/nav.tsx
+++ client/src/components/header/nav.tsx
@@ -2,8 +2,8 @@ import { NavLink } from 'react-router-dom'
 
 
 const Nav:React.FC = () => (
-<nav className='is-flex is-flex-grow-1 is-align-items-center' style={{width:'100%'}}>
-    <ul  style={{width:'100%'}}
+<nav className='is-flex is-flex-grow-1 is-align-items-center myNav myNav' style={{width:'100%'}}>
+    <ul  
             className='is-flex is-flex-direction-row is-justify-content-space-evenly is-justify-content-space-evenly'>
         <li><NavLink to='/'>Home</NavLink> </li>
 		<li><NavLink to='/confession'>Confession</NavLink></li>
