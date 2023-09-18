0fae07eb6fe63d4be88b911362564c602090c78f
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-09-18 21:34:51 +0100

    Added confession form with subject and confession validation

--- client/src/components/confession/confession.tsx
+++ client/src/components/confession/confession.tsx
@@ -1,3 +1,57 @@
-const Confession : React.FC = () => <>Confession!</>
+import { useState } from 'react'
+
+const Confession : React.FC = () => {
+	
+	
+	function textAreaChange() {
+		
+		//console.log("is is changing")
+		
+		//if(document.getElementById("confession")?.value.length > 10) 
+			//setSubmitButtonState(false)
+		
+	}
+	
+	const [subjectText, setSubjectText] = useState("")
+	const [confessionText, setConfessionText] = useState("")
+	
+	//const [submitButtonState, setSubmitButtonState] = useState(true)
+	
+	var disableSubmit = true
+	
+	if((confessionText.length > 10) && (subjectText.length > 2 )) disableSubmit = false
+	
+	return <>
+	
+	<p>
+	its very difficult to catch people committing misdemanours so we 
+	appreciate it when citizens confess to us directly.
+	</p>
+	
+	<p>
+	However, if you're just having a hard day and need to vent then you're welcome to contact us here too. Up to you!
+	</p>
+	
+	<form>
+		Subject:<input type="text" onChange={(e) =>setSubjectText(e.target.value)} /><br/>
+		Reason for contact: <select>
+		<option>I just want to talk</option>
+		<option>Mild Public Rudeness</option>
+		<option>Speaking in a Lift</option>
+		
+		<option>Not Eating Your Vegetables</option>
+		<option>Supporting Manchester United</option>
+		
+		</select> <br/>
+	
+		<textarea rows={10} cols={50} onChange={(e) =>setConfessionText(e.target.value)}>
+		
+		</textarea><br/>
+		
+		<button type="submit" disabled={disableSubmit} >Confess</button>
+	</form>
+	</>
+	
+}
 
 export default Confession
